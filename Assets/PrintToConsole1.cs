dcc6260225bbdb541dca40528c185542fa2ecd33
Jared Shurtleff <shurtl056@morrisville.edu>
2022-02-01 16:07:16 -0500

    Second commit with changed to objects in the game.

---
+++ Assets/PrintToConsole1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Okay, par four. 300 yards to the hole");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
